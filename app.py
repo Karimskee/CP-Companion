66892bd48c9473b6609279dd4dd7b111ab0e636f
Karimskee <kareemgamer2006@gmail.com>
2025-07-24 10:45:40 +0300

    Full 0.5.0
    - Login
    - Register
    - Logout

--- app.py
+++ app.py
@@ -17,15 +17,32 @@ Output:
 # flask run --debug
 
 
-from flask import Flask, redirect, render_template, request, session
+from cs50 import SQL
+
+from flask import Flask, flash, redirect, render_template, request, session
+# from flask_modals import Modal, render_template_modal
 from flask_session import Session
+
 from helpers import *
+from werkzeug.security import check_password_hash, generate_password_hash
 
 
 app = Flask(__name__)
+# modal = Modal(app)
+app.secret_key = "very top secret key mr sirrrr"
+
+# Session is 31 days by default (remind user to create an account)
+app.config["SESSION_PERMANENT"] = True
+# Store session data in server files
+app.config["SESSION_TYPE"] = "filesystem"
+# Initialize the session
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///users.db")
 
 
-# Topics
+# All available topics
 groups = {
     "C++ Fundamentals": [
         "Data Types",
@@ -79,12 +96,6 @@ groups = {
 }
 
 
-# Session is 31 days by default (remind user to create an account)
-app.config["SESSION_PERMANENT"] = True
-# Store session data in server files
-app.config["SESSION_TYPE"] = "filesystem"
-
-
 @app.route("/")
 def index():
     """Root route, redirects to the home page."""
@@ -97,10 +108,38 @@ def home():
     return render_template("home.html", page="home")
 
 
-@app.route("/create")
+@app.route("/create", methods=["GET", "POST"])
 def create():
     """Roadmap create page, where the user can enter input knowledge level."""
-    return render_template("create.html", groups=groups, page="create")
+    # Requested via navigation
+    if request.method == "GET":
+        print("GET")
+        return render_template("create.html", groups=groups, page="create")
+
+    # Requested via the create button
+    codeforces = request.form.get("codeforces")
+    atcoder = request.form.get("atcoder")
+    leetcode = request.form.get("leetcode")
+    rating = 0
+
+    # Validate input
+    if codeforces or atcoder or leetcode:
+        try:
+            codeforces = int(codeforces)
+            atcoder = int(atcoder)
+            leetcode = int(leetcode)
+        except ValueError:
+            return render_template("error.html", message="Nope")
+
+        rating = max(codeforces, atcoder, leetcode)
+
+    # Cache user data into the session (topics, rating)
+    topics = request.form.getlist("topics")
+
+    session["rating"] = rating
+    session["topics"] = topics
+
+    return redirect("/progress")
 
 
 @app.route("/progress")
@@ -136,16 +175,93 @@ def profile():
 @app.route("/logout")
 def logout():
     """Logout page, where the user can log out of their account."""
-    return render_template("logout.html")
+    # Logout user, without deleting the session stored data (e.g. topics, rating)
+    session.pop("user_id")
+    return redirect("/")
 
 
-@app.route("/login")
+# TODO: Make the login and register modals rather than pages
+@app.route("/login", methods=["GET", "POST"])
 def login():
     """Login page, where the user can log in to their account."""
-    return render_template("login.html")
+    # If accessed via navigation
+    if request.method == "GET":
+        return render_template("login.html")
+
+    # Accessed via the login button
+    email = request.form.get("email")
+    password = request.form.get("password")
+
+    print(email, password)
+
+    # Validate input
+    # TODO: When midifying the required attribute via devtools, it sends a GET request 404, leading
+    # to a blank login page FIX IT
+    if not email or not password:
+        print("missing")
+        return error("Missing required input.")
+
+    # Ensure user is registered
+    user_data = db.execute("SELECT * FROM users WHERE email = ?", email)
+
+    print(user_data)
+
+    if len(user_data) != 1:
+        # TODO: if more than 1 email found, our problem
+        print("user not found")
+        return error("email is not registered.")
+
+    # Ensure password is correct
+    if not check_password_hash(user_data[0]["password"], password):
+        print("incorrect password")
+        return error("Incorrect password.")
+
+    print("all good")
+
+    # Remember which user has logged in
+    session["user_id"] = user_data[0]["id"]
+
+    return redirect("/")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
     """Register page, where the user can create a new account."""
-    return render_template("register.html")
+    # If accessed via navigation
+    if request.method == "GET":
+        return render_template("register.html")
+
+    # Accessed via the register button
+    name = request.form.get("name")
+    email = request.form.get("email")
+    password = request.form.get("password")
+    repassword = request.form.get("repassword")
+
+    # Validate input
+    # name is not required
+    if not name:
+        name = "user"
+
+    # Required fields
+    if not email or not password or not repassword:
+        return error("Missing required input.")
+
+    # Password confirmation validation
+    if password != repassword:
+        return error("Passwords do not match.")
+
+    # check if email is available
+    user_data = db.execute("SELECT * FROM users WHERE email = ?", email)
+
+    if len(user_data) != 0:
+        return error("this email is already registered, try to login instead.")
+
+    # Insert user data into the database
+    db.execute("INSERT INTO users (name, email, password) VALUES (?, ?, ?)",
+               name, email, generate_password_hash(password))
+
+    # Log the user in
+    session["user_id"] = db.execute(
+        "SELECT id FROM users WHERE email = ?", email)[0]["id"]
+
+    return redirect("/")
--- helpers.py
+++ helpers.py
@@ -1,11 +1,42 @@
 """
 Self made helpers functions for the CP Companion website application.
 """
+from flask import flash, redirect, render_template, session
 
 
 def logged_in():
-    """
-    Check if the user is logged in, so he can access certain pages if not.
-    """
-    # Placeholder for actual login check logic
-    return True  # Assume user is logged in for now
+    """Check if the user is logged in, so he can access certain pages if not."""
+    if not session.get("user_id"):
+        return redirect("/login")
+
+    return True
+
+
+def error(message):
+    """Error feedback."""
+    return render_template("error.html", message=message)
+
+
+def apology(message, code=400):
+    """Render message as an apology to user."""
+
+    def escape(s):
+        """
+        Escape special characters.
+
+        https://github.com/jacebrowning/memegen#special-characters
+        """
+        for old, new in [
+            ("-", "--"),
+            (" ", "-"),
+            ("_", "__"),
+            ("?", "~q"),
+            ("%", "~p"),
+            ("#", "~h"),
+            ("/", "~s"),
+            ('"', "''"),
+        ]:
+            s = s.replace(old, new)
+        return s
+
+    return render_template("error.html", message=message), code
